a262a947a280b0f6c3c37a6efacc2fb9160dab12
Dillon Beliveau <dillonbeliveau@gmail.com>
2016-07-16 15:48:20 -0400

    Successfully connects to a running Dwarf Fortress

--- src/main/java/com/dillonbeliveau/DFMineApplication.java
+++ src/main/java/com/dillonbeliveau/DFMineApplication.java
@@ -2,11 +2,13 @@ package com.dillonbeliveau;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by dillon on 7/16/16.
  */
 @SpringBootApplication
+@EnableScheduling
 public class DFMineApplication {
     public static void main(String[] args) {
         SpringApplication.run(DFMineApplication.class, args);
--- src/main/java/com/dillonbeliveau/dfhack/DFConnection.java
+++ src/main/java/com/dillonbeliveau/dfhack/DFConnection.java
@@ -1,13 +1,82 @@
 package com.dillonbeliveau.dfhack;
 
+import lombok.Getter;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.Charset;
+import java.util.Arrays;
 
 @Component
 public class DFConnection {
+    @Value("${dfhack.host}")
+    String host;
+    @Value("${dfhack.port}")
+    Integer port;
+
+    private final byte[] REQUEST_MAGIC = "DFHack?\n".getBytes(Charset.forName("ASCII"));
+    private final byte[] RESPONSE_MAGIC = "DFHack!\n".getBytes(Charset.forName("ASCII"));
+    private final Integer PROTOCOL_VERSION = 1;
+
+    Logger log = LogManager.getLogger(DFConnection.class);
+
+    Socket connection;
+    OutputStream connection_output;
+    InputStream connection_input;
+
+    @Getter
+    boolean connected = false;
+
+    @Scheduled(fixedDelay = 5000)
+    private void attemptConnection() {
+        if (!connected) {
+            log.info("Attempting to connect to DF...");
+            try {
+                connection = new Socket(host, port);
+                connection_output = connection.getOutputStream();
+                connection_input = connection.getInputStream();
+
+                byte[] request = ByteBuffer
+                        .allocate(4 + REQUEST_MAGIC.length)
+                        .order(ByteOrder.LITTLE_ENDIAN)
+                        .put(REQUEST_MAGIC)
+                        .putInt(PROTOCOL_VERSION)
+                        .array();
+
+                connection_output.write(request, 0, request.length);
+                connection_output.flush();
+
+                byte[] buffer = new byte[RESPONSE_MAGIC.length];
+
+                connection_input.read(buffer, 0, buffer.length);
+
+                if (Arrays.equals(buffer, RESPONSE_MAGIC)) {
+                    connected = true;
+                    log.info("Connection successful!");
+                }
+                else {
+                    log.info("Received incorrect response magic.");
+                    connected = false;
+                }
+
+            } catch (IOException e) {
+                connected = false;
+                log.info("Connection failed.");
+            }
+        }
+    }
+
     @PostConstruct
     private void init() {
-
     }
 }
