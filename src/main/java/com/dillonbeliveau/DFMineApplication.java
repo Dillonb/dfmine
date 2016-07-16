93d41cf969d96b6bbb900610b7fcba66c41e8134
Dillon Beliveau <dillonbeliveau@gmail.com>
2016-07-16 14:29:46 -0400

    Initial commit

---
+++ src/main/java/com/dillonbeliveau/DFMineApplication.java
@@ -0,0 +1,14 @@
+package com.dillonbeliveau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dillon on 7/16/16.
+ */
+@SpringBootApplication
+public class DFMineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DFMineApplication.class, args);
+    }
+}
---
+++ src/main/java/com/dillonbeliveau/dfhack/DFConnection.java
@@ -0,0 +1,13 @@
+package com.dillonbeliveau.dfhack;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class DFConnection {
+    @PostConstruct
+    private void init() {
+
+    }
+}
